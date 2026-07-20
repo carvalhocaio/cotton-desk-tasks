3503f7ab35a476699d2d295025b0fc55bbd9a60d
Caio Carvalho <caiocarvalho.py@gmail.com>
2026-07-20 11:37:08 -0400

    Add default task backend configuration

--- config/settings.py
+++ config/settings.py
@@ -117,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+TASKS = {
+    "default": {
+        "BACKEND": "django.tasks.backends.immediate.ImmediateBackend",
+    }
+}
